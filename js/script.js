78f0d510eee0d07b6919c75a9fb11cffb97a126c
giuliaks01 <137574031+giuliaks01@users.noreply.github.com>
2023-07-07 19:05:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-
+function decimalParaBinario(decimal) {
+    return parseInt(decimal).toString(2);
+}
+function binarioParaDecimal(binario) {
+    var decimal = 0;
+    var posicao = 0;
+    while (binario > 0) {
+        var digito = binario % 10;
+        decimal += digito * Math.pow(2, posicao);
+        binario = Math.floor(binario / 10);
+        posicao++;
+    }
+    return decimal;
+}
+function decimalParaHexadecimal(decimal) {
+    return parseInt(decimal).toString(16).toUpperCase();
+}
+function hexadecimalParaDecimal(hexadecimal) {
+    return parseInt(hexadecimal, 16);
+}
